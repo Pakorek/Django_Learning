ac99c706555dc08e30208ec50e69ea72fa4729af
pakorek <brice.darmenia@sfr.fr>
2020-08-23 12:53:26 +0200

    create first template : date.html

--- crepes_bretonnes/blog/views.py
+++ crepes_bretonnes/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def home(request):
     return HttpResponse('<h1>Welcome Dude</h1>')
+
+def date_actuelle(request):
+    return render(request, 'blog/date.html', {'date': datetime.now()})
