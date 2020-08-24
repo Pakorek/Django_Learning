0abf9816bb5263d7311e07ab691a476d0e9fccbd
pakorek <brice.darmenia@sfr.fr>
2020-08-24 16:18:36 +0200

    add form constraint

--- crepes_bretonnes/blog/forms.py
+++ crepes_bretonnes/blog/forms.py
@@ -6,3 +6,26 @@ class ContactForm(forms.Form):
     message = forms.CharField(widget=forms.Textarea)
     _from = forms.EmailField(label="Votre adresse email")
     copy = forms.BooleanField(help_text="Cochez si vous souhaitez obtenir une copie du mail envoyé", required=False)
+
+    # # validation message
+    # def clean_message(self):
+    #     message = self.cleaned_data['message']
+    #     if "pizza" in message:
+    #         raise forms.ValidationError("On ne veut pas entendre parler de pizza !")
+    #
+    #     return message
+
+    # validation titre & message
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        sujet = cleaned_data.get('subject')
+        message = cleaned_data.get('message')
+
+        if sujet and message:
+            if "pizza" in sujet and "pizza" in message:
+                self.add_error("message",
+                               "Vous parlez déjà de pizzas dans le sujet, "
+                               "n'en parlez plus dans le message !"
+                               )
+
+        return cleaned_data
