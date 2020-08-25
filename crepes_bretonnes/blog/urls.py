83c0349e9564b96d7ce1fbc92be09ddafebb1b30
pakorek <brice.darmenia@sfr.fr>
2020-08-25 13:14:14 +0200

    create TemplateView for FAQ in blog/urls.py

--- crepes_bretonnes/blog/urls.py
+++ crepes_bretonnes/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name='blog_index'),
+    path(r'^faq$', TemplateView.as_view(template_name='blog/faq.html')),
     path('article/new', views.add_article, name="add_article"),
     path('article/edit/<int:id>', views.edit_article, name="edit_article"),
     path('article/<int:id>-<slug:slug>', views.show_article, name='show_article'),
--- crepes_bretonnes/crepes_bretonnes/views.py
+++ crepes_bretonnes/crepes_bretonnes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def home(request):
     return render(request, 'base.html')
+
